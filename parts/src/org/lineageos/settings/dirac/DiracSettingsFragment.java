crdroidandroid/android_device_xiaomi_sm8150-common

sm8150-common: parts: Disable Hi-Fi preference when toggling switchbar
@neobuddy89: Adapt to use mDiracUtils.

Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
Change-Id: Icaa970bace421353ddd10825d084d1124cf45275

parts/src/org/lineageos/settings/dirac/DiracSettingsFragment.java
@@ -74,6 +74,7 @@ public class DiracSettingsFragment extends PreferenceFragment implements
         boolean hifiEnable = mDiracUtils != null ? mDiracUtils.getHifiMode() : false;
         mHeadsetType.setEnabled(!hifiEnable && enhancerEnabled);
         mPreset.setEnabled(!hifiEnable && enhancerEnabled);
+        mHifi.setEnabled(mDiracUtils != null && enhancerEnabled);
     }
 
     @Override
@@ -104,9 +105,13 @@ public class DiracSettingsFragment extends PreferenceFragment implements
 
         if (mDiracUtils == null) return;
         mDiracUtils.setEnabled(isChecked);
-        if (!mDiracUtils.getHifiMode()) {
-            mHeadsetType.setEnabled(isChecked);
-            mPreset.setEnabled(isChecked);
+        mHifi.setEnabled(isChecked);
+        mHeadsetType.setEnabled(isChecked);
+        mPreset.setEnabled(isChecked);
+
+        if (!isChecked){
+            mHifi.setChecked(false);
+            mDiracUtils.setHifiMode(0);
         }
     }
 }

baalgx/device_xiaomi_sm8150-common

Signed-off-by: Joey Huab <joey@evolution-x.org>

parts/src/org/lineageos/settings/dirac/DiracSettingsFragment.java
@@ -67,6 +67,7 @@ public class DiracSettingsFragment extends PreferenceFragment implements
         boolean hifiEnable = DiracUtils.getHifiMode();
         mHeadsetType.setEnabled(!hifiEnable && enhancerEnabled);
         mPreset.setEnabled(!hifiEnable && enhancerEnabled);
+        mHifi.setEnabled(enhancerEnabled);
     }
 
     @Override
@@ -95,10 +96,13 @@ public class DiracSettingsFragment extends PreferenceFragment implements
         mSwitchBar.setChecked(isChecked);
 
         DiracUtils.setMusic(isChecked);
+        mHifi.setEnabled(isChecked);
+        mHeadsetType.setEnabled(isChecked);
+        mPreset.setEnabled(isChecked);
 
-        if (!DiracUtils.getHifiMode()) {
-            mHeadsetType.setEnabled(isChecked);
-            mPreset.setEnabled(isChecked);
+        if (!isChecked){
+            mHifi.setChecked(false);
+            DiracUtils.setHifiMode(0);
         }
     }
 }