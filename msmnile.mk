1e5d4e798defcbe13f111b78c9e81f6380dd55d5
baalgx <bal.prxgm1407@gmail.com>
2022-07-27 23:34:05 -0500

    sm8150-common: Bring QTI BT

--- msmnile.mk
+++ msmnile.mk
@@ -107,6 +107,8 @@ PRODUCT_COPY_FILES += \
 
 # Bluetooth
 PRODUCT_PACKAGES += \
+    BluetoothQti \
+    libbthost_if \
     android.hardware.bluetooth@1.1.vendor \
     vendor.qti.hardware.bluetooth_audio@2.0.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
