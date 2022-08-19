f7f33029279fadad591c68ca710ba693955363f8
daniml3 <daniel@danielml.dev>
2022-08-19 13:56:19 +0200

    sm8150-common: Add GetPreviewImageData shim for camera
    Also squashed:
        From: Arian <arian.kulmer@web.de>
        Date: Sat, 20 Aug 2022 00:31:27 +0200
        Subject: sm8150-common: Improve camera shim
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- msmnile.mk
+++ msmnile.mk
@@ -119,6 +119,7 @@ PRODUCT_PACKAGES += \
     libdng_sdk.vendor \
     libgui_vendor \
     libstdc++.vendor \
+    libpiex_shim \
     vendor.qti.hardware.camera.device@1.0.vendor \
     vendor.qti.hardware.camera.postproc@1.0.vendor
 
